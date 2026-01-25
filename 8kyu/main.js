d2d94902d6a7cd89b37bc12e03bdc9e29b45298f
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-01-24 20:03:11 -0500

    Add codewars 8kyu even or odd solution

---
+++ 8kyu/main.js
@@ -0,0 +1,11 @@
+//CodeWars: Even or Odd
+//Difficulty: 8kyu
+//Concepts: Modulo Operator
+//What I practiced: Conditionals and clean returns
+
+
+function evenOrOdd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd';
+}
+
+
