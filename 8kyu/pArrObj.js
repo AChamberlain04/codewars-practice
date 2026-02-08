16759952f66052cacdbf9337ffa22d3d7a745b9b
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-02-07 20:06:19 -0500

    Added new 8kyu solution

---
+++ 8kyu/pArrObj.js
@@ -0,0 +1,12 @@
+//You are trying to put a hash in ruby or an object in javascript or java into an array, but always returns error. solve it and keep it as simple as possible
+
+items = []
+items.push({a: "b", c: "d"});
+
+
+
+//solution
+
+//items.push ({a: "b", c: "d"});
+
+
