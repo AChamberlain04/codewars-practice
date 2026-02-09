47d3f992e090ec31dda68492d778ee42e4160348
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-02-09 17:42:22 -0500

    Added Century from Year 8kyu solution

---
+++ 8kyu/mathCeil.js
@@ -0,0 +1,5 @@
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc. Task: given a year, return the century it is in.
+
+function century(year) {
+  return Math.ceil(year/100); //using ceiling method to round up to nearest century (100)
+}
