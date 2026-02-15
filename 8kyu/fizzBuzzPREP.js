e9b30347cfaf0b97370ecc10a83d9c3f6a59b2ff
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-02-15 08:34:02 -0500

    Added PREP fizzBuzz

---
+++ 8kyu/fizzBuzzPREP.js
@@ -0,0 +1,15 @@
+//PREP//
+//A number 1 to number. 3 fizz, 5 buzz, 3&5 fizzbuzz.
+//P: number.whole number. positive number. 
+//R: console log of each number.
+//Example: 
+function fizzBuzz(num){
+    //loop
+    //conditionals % 3&5, %3, %5
+    //console.log num or fizz, buzz, fizzBuzz
+
+}
+
+fizzBuzz(5)//1,2,fizz,4, buzz
+fizzBuzz(3)//1,2,,fizz
+fizzBuzz(15)//1,2,fizz,4,buzz,fizz,7,8,fizz,buzz,11,fizz,13,14,fizzbuzz
