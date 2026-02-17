ce7d8f71b97290cd1353c1064eaf77f523b9d15f
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-02-17 18:12:10 -0500

    Added 8kyu string double task and solution

---
+++ 8kyu/strCharDouble.js
@@ -0,0 +1,16 @@
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.    
+
+//Example:
+//"String"      -->  "SSttrriinngg"
+
+
+
+
+
+function doubleChar(str) {
+  let result = "";
+  for(let i = 0; i < str.length; i++){
+    result += str[i] + str[i];
+  }
+  return result
+}
