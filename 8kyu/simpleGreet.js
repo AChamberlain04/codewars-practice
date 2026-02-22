68644de039a06fd8ff703b532fe5cd991dfb9642
Adam Chamberlain <a.chamberlain04@gmail.com>
2026-02-22 09:04:09 -0500

    Added 8kyu simple greet for easy practice

---
+++ 8kyu/simpleGreet.js
@@ -0,0 +1,7 @@
+//Create a function that accepts a paramenter name and returns a greeting in the form of "Hello, <name> how are you doing today?".
+function greet(name) {
+  return `Hello, ${name} how are you doing today?`;
+}
+
+// or
+const greet = name => `Hello, ${name} how are you doing today?`;
